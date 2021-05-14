72add1f45489188c3c675c4498c94a793e88e095
Avadhut Kesarkar <avadhoot.kesarkar@gmail.com>
2021-05-15 00:09:01 +0530

    Update ping_v1.php

--- ping_PHP/ping_v1.php
+++ ping_PHP/ping_v1.php
@@ -1,6 +1,6 @@
 <?php
 // ping Ip add.
 	$ip = "8.8.8.8";
-	$ping = exec("ping -n 4 $ip",$output,$status);
+	$ping = exec("ping -n 1 $ip",$output,$status);
 	echo $status; // 0 : success 1: fail
 ?>
